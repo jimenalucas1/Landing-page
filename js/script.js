cd92c82a07f2d204c746825fa1880d93cb4d9767
Jimena Lucas <157933753+jimenalucas1@users.noreply.github.com>
2025-06-29 21:38:24 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -152,18 +152,4 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// Contador de visitas usando CountAPI
-fetch('https://counterapi.dev/api/v1/team/ana/counter/matriculafiee261/increase', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
-  .then(res => res.json())
-  .then(data => {
-    document.getElementById('visit-counter').textContent = data.value;
-  })
-  .catch(error => {
-    console.error('Error al contar visitas:', error);
-  });
 
